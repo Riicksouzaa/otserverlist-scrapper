7ecac92ec763afcbb46fdf24b031f864702ee450
Ricardo Souza <souzaariick@gmail.com>
2020-09-07 20:00:13 -0300

    heroku needs a open webserver

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import express from "express"
 import AgendaTasks from "./tasks/agenda"
 import ParseHtmlFromOtlist from "./utils/html/parsehtml"
 
 const parseHtml = new ParseHtmlFromOtlist()
+const app = express()
 parseHtml.euViGnomos()
 
 const task = new AgendaTasks()
 task.ServerTask()
 task.NewServerTask()
+
+app.listen(process.env.PORT || 3333)
